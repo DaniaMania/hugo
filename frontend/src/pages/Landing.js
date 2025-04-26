1bfb316ac94b49cc8feee007ab9cf6729878e632
Janice Nguyen <haidn@uci.edu>
2025-04-25 23:23:26 -0700

    add basic mesh background

---
+++ frontend/src/pages/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing() {
+    return (
+        <div className="flex flex-col h-screen w-full landing-background">
+        <h1 className="text-center text-6xl text-white">HUGO</h1>
+        <p>This is the landing page of our application.</p>
+        </div>
+    );
+    }
+export default Landing;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        landing: `
+        radial-gradient(at 100% 100%, rgb(55, 48, 163) 0, transparent 52%), radial-gradient(at 83% 100%, rgb(233, 213, 255) 0, transparent 29%), radial-gradient(at 0% 100%, rgb(30, 64, 175) 0, transparent 36%), radial-gradient(at 15% 100%, rgb(165, 180, 252) 0, transparent 40%);`
+  ,
+      },
+    },
   },
   plugins: [],
-}
-
+};
