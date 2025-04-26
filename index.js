f7ae95354932a7990fdf4a9988cb403f7f28aa4e
bliz-ei <deathboi8723@gmail.com>
2025-04-25 21:25:15 -0700

    Working text prompt

---
+++ index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+
+dotenv.config();
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+async function main() {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: "How does AI work?",
+    });
+    console.log(response.text);
+}
+
+await main();
